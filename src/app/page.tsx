441534bc7794023f0207b9fae5a7970a79bd5be4
olumayokun olayinka <olumayokunolayinka@gmail.com>
2024-05-20 01:35:17 +0100

    assessment done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,286 @@
+"use client"
+
+import React, {useState, useEffect} from 'react';
 import Image from "next/image";
+import Link from "next/link";
+import moment from "moment";
+
+import {
+  BsHouse,
+  BsReceipt,
+  BsGraphUp,
+  BsBox,
+  BsGear,
+  BsBoxArrowLeft,
+  BsBell,
+  BsPersonCircle,
+  BsArrowUpRight,
+  BsArrowDownRight,
+  BsChevronDown
+} from 'react-icons/bs'
 
 export default function Home() {
+
+  interface Transaction{
+    formattedAmount: string,
+    reference: string,
+    date: string,
+    type: 'inward' | 'outward',
+    amount: number
+  }
+
+  const [transactions, setTransaction] = useState<Transaction[]>([])
+  const [filterBy, setFilterBy] = useState<string>('daily')
+  const [totalAmount, setTotalAmount] = useState<number>(0)
+  const [totalInwardAmount, setTotalInwardAmount] = useState<number>(0)
+  const [totalOutwardAmount, setTotalOutwardAmount] = useState<number>(0)
+  interface RECEIVER_TYPE{
+    name: string
+  };
+
+  const receivers : RECEIVER_TYPE [] = [
+    {name: 'Felix Vincent'},
+    {name: 'Jonah Musa'},
+    {name: 'Jide Taiwo'},
+    {name: 'Faith M'}
+  ];
+
+  type FilterBy = 'daily' | 'monthly' | 'yearly';
+
+  
+  const transactionsData : Transaction [] = [
+    {formattedAmount: '10,000.00', amount: 10000, reference: '007', date: moment().format('DD/MM/YY'), type: 'inward'},
+    {formattedAmount: '15,000.00', amount: 15000, reference: '006', date: moment().format('DD/MM/YY'), type: 'outward'},
+    {formattedAmount: '20,000.00', amount: 20000, reference: '005', date: '18/05/24', type: 'outward'},
+    {formattedAmount: '1,000.00', amount: 1000, reference: '004', date: '17/05/24', type: 'inward'},
+    {formattedAmount: '25,000.00', amount: 25000, reference: '003', date: '15/05/24', type: 'inward'},
+    {formattedAmount: '50,000.00', amount: 50000, reference: '002', date: '15/02/24', type: 'outward'},
+    {formattedAmount: '5,000.00', amount: 5000, reference: '001', date: '25/01/24', type: 'inward'}
+  ];
+
+  type onFilterByProp = (event: React.ChangeEvent<HTMLSelectElement>) => void
+
+  const onFilterBy : onFilterByProp = (event) => {
+    const selectedCriteria = event.target.value as FilterBy;
+    setFilterBy(selectedCriteria);
+    calculateMetric();
+    allFilteredTransactions();
+    
+};
+
+  type Filter = () => void
+
+  const allFilteredTransactions : Filter = () => {
+
+    const now = moment();
+
+    const filteredTransactions : Transaction [] = transactionsData.filter(transaction => {
+      const transactionDate = moment(transaction.date, 'DD/MM/YY');
+
+        switch (filterBy) {
+            case 'daily':
+                return transactionDate.isSame(now, 'day');
+            case 'monthly':
+                return transactionDate.isSame(now, 'month');
+            case 'yearly':
+                return transactionDate.isSame(now, 'year');
+            default:
+                return true;
+        }
+    });
+
+    setTransaction(filteredTransactions)
+  }
+
+  
+  type CalculateMetric = () => void
+  const calculateMetric : CalculateMetric = () => {
+    let totalAmount = 0;
+    let totalInwardAmount = 0;
+    let totalOutwardAmount = 0;
+    transactions.map(transaction => totalAmount += transaction.amount);
+    transactions.filter(transaction => transaction.type === 'inward').map(transaction => totalInwardAmount += transaction.amount);
+    transactions.filter(transaction => transaction.type === 'outward').map(transaction => totalOutwardAmount += transaction.amount);
+    setTotalAmount(totalAmount);
+    setTotalInwardAmount(totalInwardAmount);
+    setTotalOutwardAmount(totalOutwardAmount);
+  }
+
+  useEffect(() => {
+    allFilteredTransactions();
+    calculateMetric()
+  }, [transactions]);
+
+  
+  type numberWithCommasProp = (input:string) => string
+  const numberWithCommas : numberWithCommasProp = (input) => {
+    let number = parseFloat(input);
+  
+    let hasDecimal = input.toString().split('').indexOf('.') !== -1;
+
+    if (!hasDecimal) {
+      input += '.00';
+      number = parseFloat(input);
+    }
+  
+    let parts = input.toString().split('.');
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  
+    return parts.join('.');
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="md:flex gap-4 p-5 min-h-screen">
+      <div className="w-1/5 hidden md:inline bg-white rounded-lg shadow-sm py-4 min-h-screen relative text-[16px]">
+          <div className="text-gray-900 px-4 font-bold">
+            Cashir
+          </div>
+          {/* Menu  */}
+          <div className="grid grid-cols-1 gap-3 mt-5 px-4">
+            <Link href={'/'} className="text-gray-900 bg-blue-100 p-4 rounded-lg flex space-x-2 font-bold items-center">
+                <span>
+                    <BsHouse/>
+                </span>
+                <span>Home</span>
+            </Link>
+            <Link href={'#'} className="text-gray-900 p-4 rounded-lg flex space-x-2 hover:bg-blue-100 font-light items-center">
+                <span><BsReceipt/></span>
+                <span>Transactions</span>
+            </Link>
+            <Link href={'#'} className="text-gray-900 p-4 rounded-lg flex space-x-2 hover:bg-blue-100 font-light items-center">
+                <span><BsGraphUp/></span>
+                <span>Analytics</span>
+            </Link>
+            <Link href={'#'} className="text-gray-900 p-4 rounded-lg flex space-x-2 hover:bg-blue-100 font-light items-center">
+                <span><BsBox/></span>
+                <span>Products</span>
+            </Link>
+          </div>
+          <div className="absolute bottom-10 w-full px-4">
+            <div className="grid grid-cols-1 gap-3">
+              <Link href={'#'} className="text-gray-900 p-4 rounded-lg flex space-x-2 hover:bg-blue-100 font-light items-center">
+                  <span><BsGear/></span>
+                  <span>Settings</span>
+              </Link>
+              <Link href={'#'} className="text-red-600 p-4 rounded-lg flex space-x-2 hover:bg-blue-100 font-light items-center">
+                  <span><BsBoxArrowLeft/></span>
+                  <span>Log out</span>
+              </Link>
+            </div>
+          </div>
+          {/* Menu  */}
+      </div>
+      <div className="md:w-4/5">
+        <div className="bg-white rounded-lg shadow-sm p-4 flex items-center justify-between">
+            <div className="text-gray-900 text-sm">Hello, <strong>Olumayokun</strong></div>
+            <div className="flex space-x-3 items-center">
+                <div className="bg-gray-200 w-5 h-5 p-5 flex justify-center items-center rounded-[10px] text-gray-900 cursor-pointer">
+                  <span><BsBell/></span>
+                </div>
+                <div className="text-gray-200">|</div>
+                <div className="flex space-x-2">
+                  <div className="bg-green-500 w-5 h-5 p-5 flex justify-center items-center rounded-[10px] text-white">
+                    <span><BsPersonCircle/></span>
+                  </div>
+                  <div>
+                      <h2 className="text-gray-900 text-[14px] font-light">Olumayokun</h2>
+                      <h2 className="text-gray-500 text-[12px] font-light">ID:11010101</h2>
+                  </div>
+                </div>
+            </div>
+        </div>
+        <div className='mt-5'>
+            <label className='text-gray-900 px-3 font-light text-sm py-2 rounded-full border w-[100px] h-[35px] bg-white relative flex items-center cursor-pointer'>
+              <select value={filterBy} onChange={onFilterBy}
+                      className='border-none appearance-none bg-transparent outline-none capitalize cursor-pointer absolute w-full h-full'>
+                  <option>all</option>
+                  <option value={'daily'}>daily</option>
+                  <option value={'monthly'}>monthly</option>
+                  <option value={'yearly'}>yearly</option>
+              </select>
+              <div className='text-gray-900 absolute right-2'><BsChevronDown/></div>
+            </label>
+        </div>
+        <div className="md:flex gap-4 mt-5 space-y-5 md:space-y-0">
+          <div className="md:w-2/3 grid-cols-1 grid gap-4">
+            <div className="bg-white rounded-lg shadow-sm p-4">
+                <div className="mb-3">
+                  <h3 className="text-gray-900 font-bold text-sm">Total amount</h3>
+                </div>
+                <h3 className="font-light text-gray-900 text-[1.5em] mb-1">
+                  ₦{numberWithCommas(`${totalAmount}`)}
+                </h3>
+                <p className="text-gray-900 text-[11px] font-light">Inward ₦50,000.00 this month</p>
+            </div>
+            <div className="md:flex gap-4 space-y-5 md:space-y-0">
+              <div className="bg-white rounded-lg shadow-sm p-4 md:w-1/2 flex items-center h-fit">
+                <div>
+                <div className="mb-3">
+                    <h3 className="text-gray-900 font-bold text-sm">Total inward</h3>
+                  </div>
+                  <h3 className="font-light text-gray-900 text-[1.5em] mb-1">
+                    ₦{numberWithCommas(`${totalInwardAmount}`)}
+                  </h3>
+                  <p className="text-gray-900 text-[11px] font-light">5% increased this week</p>
+                </div>
+              </div>
+              <div className="bg-white rounded-lg shadow-sm p-4 md:w-1/2 flex items-center h-fit">
+                <div>
+                <div className="mb-3">
+                    <h3 className="text-gray-900 font-bold text-sm">Total outward</h3>
+                  </div>
+                  <h3 className="font-light text-gray-900 text-[1.5em] mb-1">
+                    ₦{numberWithCommas(`${totalOutwardAmount}`)}
+                  </h3>
+                  <p className="text-gray-900 text-[11px] font-light">25% increased this week</p>
+                </div>
+              </div>
+            </div>
+            <div className="bg-white rounded-lg shadow-sm p-4 text-gray-900">
+                <h3 className="text-gray-900 font-bold text-sm">Recent transactions</h3>
+                <table className="table-auto w-full mt-5">
+                      <thead>
+                        <tr className="text-gray-500 font-light text-[13px]">
+                            <td>#</td>
+                            <td>Reference</td>
+                            <td>Amount</td>
+                            <td className="text-center">Type</td>
+                            <td>Date</td>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {transactions.map((transaction, index)=>(
+                          <tr key={index} className="border-t text-sm font-light">
+                          <td className="items-center">{index+1}</td>
+                          <td className="items-center">{transaction.reference}</td>
+                          <td className="h-[3em] items-center flex">₦{transaction.formattedAmount}</td>
+                          <td className="h-[3em] items-center justify-center text-center">
+                            <div className={`w-[5em] p-1 flex items-center justify-center m-auto rounded-lg text-white
+                                  ${transaction.type === 'outward' ? 'bg-red-500' : 'bg-green-500'}`}>
+                              {transaction.type === 'outward' ? (<BsArrowUpRight/>) : (<BsArrowDownRight/>)}
+                            </div>
+                          </td>
+                          <td className="items-center">{transaction.date}</td>
+                      </tr>
+                        ))}
+                      </tbody>
+                  </table>
+            </div>
+          </div>
+          <div className="md:w-1/3 grid-cols-1 grid gap-4">
+            <div className="bg-white rounded-lg shadow-sm p-4 h-fit">
+                <h3 className="text-gray-900 font-bold text-sm">Top receivers</h3>
+                <div className="mt-5 grid grid-cols-1 gap-4">
+                  {receivers.map((receiver, index)=>(
+                    <div key={index} className="text-gray-900 bg-gray-200 p-3 rounded-lg">
+                    <h3 className="text-[15px] font-light">{receiver.name}</h3>
+                  </div>
+                  ))}
+                </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
